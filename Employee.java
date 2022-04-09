f9dee0b75efebeb1ef866732066f998b51a8a326
Justin <73486390+Sombero@users.noreply.github.com>
2022-04-09 13:01:00 -0400

    Changes by Justin

--- Employee.java
+++ Employee.java
@@ -1,2 +1,20 @@
-public class Employee {
+import java.util.ArrayList;
+
+public abstract class Employee {
+    private int employeeID;
+    private String employeeName;
+
+    public void setManager(SalesManager salesManager){
+
+    }
+    public void addCustomer(Customer customer){
+
+    }
+    public ArrayList<Customer> getCustomers(ArrayList<Customer> customers){
+        return null;
+    }
+
+    public String toString(){
+        return "";
+    }
 }
