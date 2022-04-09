f1e1033336a2dab548486415272349da89ab5e8b
Justin <73486390+Sombero@users.noreply.github.com>
2022-04-09 13:01:13 -0400

    Changes by aliso

--- SalesAssociate.java
+++ SalesAssociate.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class SalesAssociate {
+    double salary;
+    SalesManager salesManager;
+    ArrayList<Employee> employees;
+
+    public SalesAssociate(double salary, SalesManager salesManager, ArrayList employees) {
+        this.salary = salary;
+        this.salesManager = salesManager;
+        this.employees = employees;
+    }
 }
